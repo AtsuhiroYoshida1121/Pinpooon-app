b32591187d29c53f9324addf6d7a65b76e526092
Atsuhiro <atsuhiro.yoshida.coding@gmail.com>
2021-06-04 20:43:56 +0900

    third commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { connected, send } = require('process');
 const Nodemailer= require('nodemailer');
 
